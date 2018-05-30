2ff7f8a9529a14e9cd756b06069822ef25138fa5
Dom Corvasce <dom.corvasce@yandex.com>
2018-05-30 18:11:58 +0200

    Add database schema

---
+++ schema.sql
@@ -0,0 +1,59 @@
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(200) NOT NULL,
+  last_name VARCHAR(200) NOT NULL,
+  email VARCHAR(200) NOT NULL,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+
+  UNIQUE(email)
+);
+
+CREATE TABLE sources (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  uri TEXT NOT NULL,
+  title TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+
+  UNIQUE(uri)
+);
+
+CREATE TABLE categories (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+
+  UNIQUE(name)
+);
+
+CREATE TABLE articles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  uri TEXT NOT NULL,
+  title TEXT NOT NULL,
+  preview TEXT NOT NULL,
+  category_id INTEGER NOT NULL REFERENCES categories(id),
+  source_id INTEGER NOT NULL REFERENCES sources(id),
+  published_at DATETIME NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+
+  UNIQUE(uri)
+);
+
+CREATE TABLE subscriptions (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  source_id INTEGER REFERENCES sources(id),
+  created_at TIMESTAMP NOT NULL,
+
+  UNIQUE(user_id, source_id)
+);
+
+CREATE TABLE user_favouritearticles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  article_id INTEGER REFERENCES articles(id),
+  created_at TIMESTAMP NOT NULL,
+
+  UNIQUE(user_id, article_id)
+);
