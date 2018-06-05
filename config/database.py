5c120d1610683972abbe7797b39777d44b5d4684
Dom Corvasce <dom.corvasce@yandex.com>
2018-06-05 11:34:31 +0200

    Enable autocommit for PyMySQL

--- config/database.py
+++ config/database.py
@@ -4,4 +4,4 @@ from os import getenv
 
 load_dotenv()
 conn = connect(getenv('DB_HOST'), getenv('DB_USER'),
-               getenv('DB_PASSWORD'), getenv('DB_NAME'), charset='utf8')
+               getenv('DB_PASSWORD'), getenv('DB_NAME'), charset='utf8', autocommit=True)
